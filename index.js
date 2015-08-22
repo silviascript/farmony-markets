3ff2290a6db0de23009e4ff8286768c2f98b0fd8
Silvia Brown <siltastic@gmail.com>
2015-08-22 18:48:11 -0400

    Connectd express to the application.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Require Express
 var express = require("express");
 
+// Connect Express to application.
+var app = Express();
+
 // Port Listener
 app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port 3000");
