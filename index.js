7f3e9150759098ffb629dfd7e92259aef5b7144a
Silvia Brown <siltastic@gmail.com>
2015-08-24 16:41:31 -0400

    Edited Heroku deployment function.

--- index.js
+++ index.js
@@ -76,8 +76,6 @@ app.get("/vendors/:id/edit", function(req,res){
 //   console.log("Listening on port 3000.");
 // });
 
-app.set('port', (process.env.PORT || 3000));
-
-app.listen(app.get('port'), function(){
- console.log("Listening on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
