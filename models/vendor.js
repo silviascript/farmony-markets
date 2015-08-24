259f50830b36a81853d86e5f09d85ff3cf2eb1ba
Silvia Brown <siltastic@gmail.com>
2015-08-24 12:14:14 -0400

    Edited data table structure. Peered S/L.

--- models/vendor.js
+++ models/vendor.js
@@ -2,12 +2,17 @@ module.exports = function(sequelize, DataTypes){
   return sequelize.definte("vendor", {
     name: DataTypes.STRING,
     address: DataTypes.STRING,
+    email: DataTypes.STRING,
     mapUrl: DataTypes.STRING,
+    phone: DataTypes.STRING,
     product: DataTypes.TEXT,
     paymentCard: DataTypes.BOOLEAN,
     paymentCash: DataTypes.BOOLEAN,
     paymentOther: DataTypes.TEXT,
     paymentSnap: DataTypes.BOOLEAN,
-    photoUrl: DataTypes.STRING
+    photoUrl: DataTypes.STRING,
+    facebookUrl: DataTypes.STRING,
+    twitterUrl: DataTypes.STRING,
+    websiteUrl: DataTypes.STRING,
   });
 }
