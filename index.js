2bd6d90cb88f3e70f3807bea6433c2613f35994b
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-08-24 10:41:16 -0400

    more file structure. peer j/o

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
 var express = require("express");
 var app = express();
 
+// Connect path middleware
+var path = require("path");
+
+
+
 // Connecting to public assets.
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(path.join(__dirname + "/public")));
+
+// set up hbs
+app.set("view engine", "hbs");
 
 // Port listener.
 app.listen(3000, function(){
