5575a4dadfcc9f196c6518ce10ecfd0a06f408af
Silvia Brown <siltastic@gmail.com>
2015-08-24 16:48:07 -0400

    Edited deployment functionality.

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,25 @@
 // Load the sequelize.
 // Connect the sequelize to the database.
 var Sequelize = require("sequelize");
-var databaseConnection = new Sequelize("postgres:///farmony_db");
+// var databaseConnection = new Sequelize("postgres:///farmony_db");
+
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  databaseConnection = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  databaseConnection = new Sequelize("postgres:///farmony_db");
+}
 
 // Create farmers market model.
-// Create vendors model.
-var Market = databaseConnection.import("../models/market");
-var Vendor = databaseConnection.import("../models/vendor");
+// // Create vendors model.
+// var Market = databaseConnection.import("../models/market");
+// var Vendor = databaseConnection.import("../models/vendor");
 
 // Vendors belong to many Farmers Markets.
 // Farmers Markets have many vendors.
--- index.js
+++ index.js
@@ -76,6 +76,8 @@ app.get("/vendors/:id/edit", function(req,res){
 //   console.log("Listening on port 3000.");
 // });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+ console.log("Listening on port 3000");
 });
