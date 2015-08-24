e68dd8fd11d5886aaa3d1a18b873924d7f254f6d
Silvia Brown <siltastic@gmail.com>
2015-08-24 10:55:16 -0400

    Created and connected database. Peered S/L.

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,12 @@
 // Load the sequelize.
+// Connect the sequelize to the database.
 var Sequelize = require("sequelize");
+var db_connection = new Sequelize("postgress:///farmony_db");
+
+// Create farmers market model.
+// Create vendors model.
+
+// Vendors belong to many Farmers Markets.
+// Farmers Markets have many vendors.
+
+// Add module exports for sequelize and models.
