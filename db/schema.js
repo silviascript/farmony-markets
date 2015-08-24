8dd79f760b9a14bdd2a030bb4928d4991c6a7081
Silvia Brown <siltastic@gmail.com>
2015-08-24 14:36:11 -0400

    Removed schema file.

--- db/schema.js
+++
@@ -1,7 +0,0 @@
-// Require the connection JavaScript file.
-var FarmonyDatabase = require("./connection");
-
-// Sync.
-FarmonyDatabase.do.sync({force: true}).then(function(){
-  process.exit();
-});
