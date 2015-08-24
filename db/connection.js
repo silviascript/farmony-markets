473afe78f08b8729b26fedf87ec74b97480ec04e
Silvia Brown <siltastic@gmail.com>
2015-08-24 16:12:59 -0400

    Edited sequelize and module exports. Peer S/L.

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 // Load the sequelize.
 // Connect the sequelize to the database.
 var Sequelize = require("sequelize");
-var db_connection = new Sequelize("postgress:///farmony_db");
+var databaseConnection = new Sequelize("postgres:///farmony_db");
 
 // Create farmers market model.
 // Create vendors model.
-var Market = sequelize.import("../models/vendor");
-var Vendor = sequelize.import("../models/vendor");
+var Market = databaseConnection.import("../models/market");
+var Vendor = databaseConnection.import("../models/vendor");
 
 // Vendors belong to many Farmers Markets.
 // Farmers Markets have many vendors.
@@ -14,9 +14,9 @@ Vendor.belongsTo(Market);
 Market.hasMany(Vendor);
 
 // Add module exports for sequelize and models.
-module.export = {
-  sql: Sequelize,
-  do: sequelize,
+module.exports = {
+  Sequelize: Sequelize,
+  sequelize: databaseConnection,
   models: {
     Vendor: Vendor,
     Market: Market
