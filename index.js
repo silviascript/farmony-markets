e6d1d7d8e3e63ead594d187a61c8e6baa552d01e
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-25 14:39:19 -0400

    refactored index.js removed routes

--- index.js
+++ index.js
@@ -57,35 +57,6 @@ app.get("/markets/:id", function(req,res){
   res.render("markets/show", {message: "Info for farmer's market " + req.params.id + "."})
 });
 
-// Index route for vendors.
-app.get("/vendors", function(req,res){
-  // Vendor.findAll().then(function(vendors){
-    res.render("vendors/index", {message: "List of vendors."})
-  // })
-});
-
-// New route for vendors.
-app.get("/vendors/new", function(req,res){
-  res.render("vendors/new", {message: "Form for new vendor."})
-});
-
-// Show route for vendors.
-app.get("/vendors/:id", function(req,res){
-  res.render("vendors/show", {message: "Info for vendor " + req.params.id + "."})
-});
-
-// Edit route for vendors.
-app.get("/vendors/:id/edit", function(req,res){
-  res.render("vendors/edit", {message: "Edit form for vendor " + req.params.id + "."})
-});
-
-// Create new vendor route.
-// app.post("/vendors", function(req,res){
-//   Vendor.create(req.body).then(function(vendor){
-//     res.json(vendor);
-//   })
-// })
-
 // Create route for about.
 app.get('/about', function(req, res){
   res.render('about', {
@@ -99,7 +70,6 @@ app.get('/contact', function(req, res){
     title: 'Contact'
   });
 });
-
 
 // Port listener.
 app.set("port", (process.env.PORT || 3000));
