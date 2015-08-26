20cfcda1879526fb4e4be9da7907814c425ed260
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-26 13:06:24 -0400

    search able to pass value

--- db/search.js
+++ db/search.js
@@ -1,21 +1,18 @@
-var Vendor = require('../models/vendor').Vendor;
-var Market = require('../models/market').Market;
+var Vendor = require("../db/connection").models.Vendor;
+var Market = require("../db/connection").models.Market;
 
 module.exports = {
     marketSearch: function(req, res){
         var userSearch = req
-        console.log(userSearch)
-
-        Market.findOne({name: userSearch['name']}, function(err, doc) {
+        console.log("working: " + Vendor)
+        Vendor.find({name: userSearch['name']}, function(err, doc) {
             if(!err && doc) {
                 res.json(200, doc);
             } else if(err) {
-                res.json(500, { message: "Error loading task." + err});
+                res.json(500, { message: "Error loading Market." + err});
             } else {
-                res.json(404, { message: "Task not found."});
+                res.json(404, { message: "Market not found."});
             }
         });
-
-        // return console.log("marketSearch path works")
     }
 }
