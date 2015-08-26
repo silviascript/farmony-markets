9b83600109cab350755b7acf554809700d585d1e
Silvia Brown <siltastic@gmail.com>
2015-08-26 19:58:27 -0400

    Added scrollTop to prevent movement.

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,22 @@
 "use strict"
 
-// Site Loader Loads & Fades Out
+// Site loader loads & fades out.
 $(window).on("load", function(){
-  $("#site-container").fadeIn(2500);
+  setTimeout(function() { // Testing use only.
+    $("#site-loader").css("display", "none");
+      $("#site-container").fadeIn(2000);
+    }, 1000);
+
+  // Site content fades in.
+  $("#site-container").fadeOut(2000);
+
+  // Site scroll to anchor.
+  $(".scroll").click(function(event) {
+    event.preventDefault();
+
+  $("html,body").animate({
+    scrollTop: $( $(this).attr("href") ).offset().top } , 1000);
+  });
 });
 
 // $( document ).ready(function() {
@@ -14,10 +28,9 @@ $(window).on("load", function(){
 //       // url: ""
 //     }).done(function(response){
 //       $(".site-content").html(response);
-//       console.log("added")
-
+//       console.log("Added content.")
 //     }).fail(function(response){
-//       console.log("ajax get request failed")
+//       console.log("AJAX GET request failed.)
 //     })
 //   });
 // });
