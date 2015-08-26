3cd3b62399910405449cdd92e750168143c6e132
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-26 15:41:05 -0400

    search works if name is an exact match

--- db/search.js
+++ db/search.js
@@ -2,17 +2,21 @@ var Vendor = require("../db/connection").models.Vendor;
 var Market = require("../db/connection").models.Market;
 
 module.exports = {
-    marketSearch: function(req, res){
-        var userSearch = req
-        console.log("working: " + Vendor)
-        Vendor.find({name: userSearch['name']}, function(err, doc) {
-            if(!err && doc) {
-                res.json(200, doc);
-            } else if(err) {
-                res.json(500, { message: "Error loading Market." + err});
-            } else {
-                res.json(404, { message: "Market not found."});
+    vendorSearch: function(req, res){
+        var userVendorSearch = req
+        Vendor.findAll({ where: {name: userVendorSearch} }).then(function(vendorResults){
+            for(i = 0; i < vendorResults.length; i++){
+            console.log(vendorResults[i].id)
             }
-        });
+        })
+    },
+
+    marketSearch: function(req, res){
+        var userMarketSearch = req
+        Market.findAll({ where: {name: userMarketSearch}}).then(function(marketResults){
+            for(i = 0; i < marketResults.length; i++){
+                console.log(marketResults[i].id)
+            }
+        })
     }
 }
