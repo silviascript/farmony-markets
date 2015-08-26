aae4f73f3d729b8063f0cee6559611197b223628
Silvia Brown <siltastic@gmail.com>
2015-08-26 19:51:41 -0400

    Added fadeIn for each page.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,23 @@
 "use strict"
 
-// Site loader and fade out.
-// $(window).on("load", function(){
-//   $("#site-container").fadeIn(2000);
+// Site Loader Loads & Fades Out
+$(window).on("load", function(){
+  $("#site-container").fadeIn(2500);
+});
+
+// $( document ).ready(function() {
+//   $("#about").click(function(event){
+//     event.preventDefault();
+
+//     $.ajax({
+//       type: "GET",
+//       // url: ""
+//     }).done(function(response){
+//       $(".site-content").html(response);
+//       console.log("added")
+
+//     }).fail(function(response){
+//       console.log("ajax get request failed")
+//     })
+//   });
 // });
