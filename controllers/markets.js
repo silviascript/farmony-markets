0124c8633182f95de6f5b3bb61d0c2caf90ae62c
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-08-27 10:32:19 -0400

    created market controller, listed markets on index page. peer o/j

---
+++ controllers/markets.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+var Market = require("../db/connection").models.Market;
+
+function error(response, message){
+  response.status(500);
+  response.json({error: message})
+}
+
+// Index route for markets.
+router.get("/markets", function(req,res){
+  Market.findAll().then(function(markets){
+    res.render("markets/index", {markets: markets})
+  })
+});
+
+// Show route for markets.
+router.get("/markets/:id", function(req,res){
+  Market.findById(req.params.id).then(function(market){
+    if(!market) return error(res, "not found");
+      res.render("markets/show", {market: market})
+  });
+});
+
+module.exports = router;
