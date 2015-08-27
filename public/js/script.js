f0f23dbd2946194ab3e7346b62b089fddef9944e
Silvia Brown <siltastic@gmail.com>
2015-08-27 00:14:24 -0400

    Added map views and basic options.

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,23 @@
 "use strict"
 
 // Site loader loads & fades out.
-$(window).on("load", function(){
-  setTimeout(function() { // Testing use only.
-    $("#site-loader").css("display", "none");
-      $("#site-container").fadeIn(2000);
-    }, 1000);
+// $(window).on("load", function(){
+  // setTimeout(function() { // Testing use only.
+  //   $("#site-loader").css("display", "none");
+  //     $("#site-container").fadeIn(2000);
+  //   }, 1000);
 
-  // Site content fades in.
-  $("#site-container").fadeOut(2000);
+  // // Site content fades in.
+  // $("#site-container").fadeOut(2000);
 
   // Site scroll to anchor.
-  $(".scroll").click(function(event) {
-    event.preventDefault();
+//   $(".scroll").click(function(event) {
+//     event.preventDefault();
 
-  $("html,body").animate({
-    scrollTop: $( $(this).attr("href") ).offset().top } , 1000);
-  });
-});
+//   $("html,body").animate({
+//     scrollTop: $( $(this).attr("href") ).offset().top } , 1000);
+//   });
+// });
 
 // $( document ).ready(function() {
 //   $("#about").click(function(event){
@@ -34,3 +34,37 @@ $(window).on("load", function(){
 //     })
 //   });
 // });
+  // var latitude = 51.5255069;
+  // var  longitude = -0.0836207;
+  // var  map_zoom = 14;
+
+  // var map_options = {
+
+    
+  //   center: new google.maps.LatLng(latitude, longitude),
+  //   zoom: map_zoom,
+  //   panControl: false,
+  //   zoomControl: false,
+  //   mapTypeControl: false,
+  //   streetViewControl: false,
+  //   scrollwheel: false,
+  // }
+
+
+$( document ).ready(function($){
+  console.log("Google Map is ready to load.");
+
+  // Set the latitude and longitude to center the map.
+  // Set the starting zoom level.
+  var latitude = 38.9047;
+  var longitude = -77.0164;
+  var mapZoom = 11;
+
+  // Set all the styles.
+
+  // Set all the map options.
+  var mapOptions = {
+    center: new google.maps.LatLng(latitude, longitude), 
+    zoom: mapZoom
+  }
+});
