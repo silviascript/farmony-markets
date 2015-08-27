4ad5d70e24fc1584af39255295944beb730b8c96
Silvia Brown <siltastic@gmail.com>
2015-08-27 13:45:28 -0400

    Added new table columns.

--- models/market.js
+++ models/market.js
@@ -1,23 +1,37 @@
 module.exports = function(sequelize, DataTypes){
   return sequelize.define("market", {
-    name: DataTypes.STRING,
     api_id: DataTypes.INTEGER,
-    addressStreet: DataTypes.STRING,
+    name: DataTypes.STRING,
+
+    // Contact Information
+    email: DataTypes.STRING,
+    facebookUrl: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    twitterUrl: DataTypes.STRING,
+    websiteName: DataTypes.STRING,
+    websiteUrl: DataTypes.STRING,
+
+    // Detailed Information
+    photoUrl: DataTypes.STRING,
+    products: DataTypes.ARRAY(DataTypes.STRING),
+    vendorsNumber: DataTypes.INTEGER,
+
+    // Location Information
     addressCity: DataTypes.STRING,
     addressState: DataTypes.STRING,
+    addressStreet: DataTypes.STRING,
     addressZip: DataTypes.STRING,
     googleLink: DataTypes.STRING,
-    products: DataTypes.ARRAY(DataTypes.STRING),
+
+    // Market Hours
     dayOpen: DataTypes.STRING,
+    monthsOpen: DataTypes.ARRAY(DataTypes.STRING),
     timeOpen: DataTypes.STRING,
     timeClose: DataTypes.STRING,
-    monthsOpen: DataTypes.ARRAY(DataTypes.STRING),
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    photoUrl: DataTypes.STRING,
-    facebookUrl: DataTypes.STRING,
-    twitterUrl: DataTypes.STRING,
-    vendorsNumber: DataTypes.INTEGER,
-    websiteUrl: DataTypes.STRING
+
+    // Payment Options
+    paymentCard: DataTypes.STRING,
+    paymentCash: DataTypes.STRING,
+    paymentSnap: DataTypes.STRING
   });
 }
