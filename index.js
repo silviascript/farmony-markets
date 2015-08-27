c299a30b2b4ae7e1422485e953874b392dae675a
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-26 21:43:42 -0400

    added search for zipcode

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var path = require("path");
 // Connect bodyparser middleware.
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // Connect Controllers
 var vendorsController = require("./controllers/vendors.js");
@@ -20,11 +22,11 @@ var search = require("./db/search.js")
 
 // Connecting to public assets.
 app.use(sassMiddleware({
-  src: __dirname  + '/public/sass/',
-  dest: path.join(__dirname, '/public/css/'),
-  debug: true,
-  outputStyle: 'compressed',
-  prefix: '/prefix'
+    src: __dirname + '/public/sass/',
+    dest: path.join(__dirname, '/public/css/'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/prefix'
 }));
 
 app.use("/public", express.static(path.join(__dirname + "/public")));
@@ -41,57 +43,79 @@ app.set("view engine", "hbs");
 app.use("/", vendorsController);
 
 // Connect to the browser.
-app.get("/", function(req, res){
-  res.render("index", {message: "Handlebars is the best."})
-  res.send("Browser time!");
+app.get("/", function(req, res) {
+    res.render("index", {
+        message: "Handlebars is the best."
+    })
+    res.send("Browser time!");
 });
 
 var Vendor = require("./db/connection").models.Vendor;
 var Market = require("./db/connection").models.Market;
 
-app.get("/search", function(req, res){
+app.get("/search", function(req, res) {
     var userMarketSearch = req.query.q
-    console.log("SEARCH: " + userMarketSearch)
-    // search.marketSearch(req.query.q)
-    Market.findAll({ where: {name: userMarketSearch}}).then(function(searchResults){
+        // search.marketSearch(req.query.q)
+    Market.findAll({
+        where: {
+            name: userMarketSearch
+        }
+    }).then(function(searchResults) {
         console.log(searchResults)
-        res.render("markets/search", {markets: searchResults})
+        if (searchResults == false) {
+            Market.findAll({
+                where: {
+                    addressZip: userMarketSearch
+                }
+            }).then(function(searchResults) {
+                res.render("markets/search", {
+                    markets: searchResults
+                })
+            })
+        } else
+            res.render("markets/search", {
+                markets: searchResults
+            })
     })
 
     // res.send("search complete")
 })
 
 // Index route for markets.
-app.get("/markets", function(req,res){
-  // console.log(apiRouter.sayHello())
-  // Market.findAll().then(function(markets){
+app.get("/markets", function(req, res) {
+    // console.log(apiRouter.sayHello())
+    // Market.findAll().then(function(markets){
     // var results = apiRouter.marketResults(20001)
-    res.render("markets/index", {message: "market route"})
-  // })
+    res.render("markets/index", {
+            message: "market route"
+        })
+        // })
 });
 
 // Show route for markets.
-app.get("/markets/:id", function(req,res){
-  res.render("markets/show", {message: "Info for farmer's market " + req.params.id + "."})
+app.get("/markets/:id", function(req, res) {
+    res.render("markets/show", {
+        message: "Info for farmer's market " + req.params.id + "."
+    })
 });
 
 // Create route for about.
-app.get('/about', function(req, res){
-  res.render('about', {
-    title: 'About'
-  });
+app.get('/about', function(req, res) {
+    res.render('about', {
+        title: 'About'
+    });
 });
 
 // Create route for contact.
-app.get('/contact', function(req, res){
-  res.render('contact', {
-    title: 'Contact'
-  });
+app.get('/contact', function(req, res) {
+    res.render('contact', {
+        title: 'Contact'
+    });
 });
 
 // Port listener.
 app.set("port", (process.env.PORT || 3000));
 
-app.listen(app.get("port"), function(){
- console.log("Listening on port 3000.");
+app.listen(app.get("port"), function() {
+    console.log("Listening on port 3000.");
 });
