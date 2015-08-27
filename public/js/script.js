645e27d8f7686e2bcdd574e1da31db3ce105e79d
Silvia Brown <siltastic@gmail.com>
2015-08-27 15:04:42 -0400

    Added scrolling preventions.

--- public/js/script.js
+++ public/js/script.js
@@ -35,8 +35,22 @@
 //   });
 // });
 
-$( document ).ready(function($){
+$(document).ready(function($){
   console.log("Google Map is ready to load.");
+
+  // Disables scrolling on site.
+  $("#site-container").bind("mousewheel", function() {
+     return false;
+  });
+
+  // Prevent double scrolling using Chrome and Firefox scrolls.
+  $(".site-markets").bind("mousewheel DOMMouseScroll", function (e){
+    var e0 = e.originalEvent,
+      delta = e0.wheelDelta || -e0.detail;
+    
+    this.scrollTop += ( delta < 0 ? 1 : -1 ) * 30;
+    e.preventDefault();
+  });
 
   // Set the latitude and longitude to center the map.
   // Set the starting zoom level.
