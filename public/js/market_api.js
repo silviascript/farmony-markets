d95bfca2044fa833aa9ec41d765ec154cdf066f4
Silvia Brown <siltastic@gmail.com>
2015-08-26 21:19:24 -0400

    Edited indentation.

--- public/js/market_api.js
+++ public/js/market_api.js
@@ -1,39 +1,39 @@
 // Farmers Market API
 
 var farmersMarketApi = {
-    sayHello: function() {
-        console.log("Hello! Market API")
-    },
+  sayHello: function() {
+    console.log("Hello! Market API.")
+  },
 
-    marketResults: function(zip) {
-        // or
-        // function getResults(lat, lng) {
-        $.ajax({
-            type: "GET",
-            contentType: "application/json; charset=utf-8",
-            // submit a get request to the restful service zipSearch or locSearch.
-            url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
-            // or
-            // url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=" + lat + "&lng=" + lng,
-            dataType: 'jsonp',
-            jsonpCallback: 'searchResultsHandler'
-        });
-    },
+  marketResults: function(zip) {
+    // Function getResults(lat, lng) {
+    $.ajax({
+      type: "GET",
+      contentType: "application/json; charset=utf-8",
 
-    //iterate through the JSON result object.
-    searchResultsHandler: function(searchResults) {
-        for (var key in searchresults) {
-            alert(key);
-            var results = searchresults[key];
-            for (var i = 0; i < results.length; i++) {
-                var result = results[i];
-                for (var key in result) {
-                    //only do an alert on the first search result
-                    if (i == 0) {
-                        alert(result[key]);
-                    }
-                }
-            }
+      // Submit a get request to the restful service zipSearch or locSearch.
+      url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
+
+      // url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=" + lat + "&lng=" + lng.
+      dataType: 'jsonp',
+      jsonpCallback: 'searchResultsHandler'
+    });
+  },
+
+  //Iterate through the JSON result object.
+  searchResultsHandler: function(searchResults) {
+    for (var key in searchresults) {
+      alert(key);
+      var results = searchresults[key];
+      for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+        for (var key in result) {
+          // Only do an alert on the first search result.
+          if (i == 0) {
+            alert(result[key]);
+          }
         }
+      }
     }
+  }
 }
