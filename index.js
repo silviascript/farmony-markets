2e28743e24935c218c217001aa4d992e0a7f52af
Silvia Brown <siltastic@gmail.com>
2015-08-27 02:02:40 -0400

    Edited indentation.

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ var path = require("path");
 // Connect bodyparser middleware.
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Connect Controllers
+var vendorsController = require("./controllers/vendors.js");
+
+// Connect search engine.
+var search = require("./db/search.js")
 
 app.use("/public", express.static(path.join(__dirname + "/public")));
 app.use(express.static('public'));
@@ -21,64 +29,77 @@ app.set("view engine", "hbs");
 // Connect to the Farmers Market API.
 // var apiRouter = require("./public/js/market_api.js")
 
+// Using connected exports.
+// Disabled until routes/views are updated.
+
+app.use("/", vendorsController);
+
 // Connect to the browser.
-app.get("/", function(req, res){
-  res.render("index", {message: "Handlebars is the best."})
-  // res.send("Browser time!");
+app.get("/", function(req, res) {
+  res.render("index", {
+    message: "Handlebars is the best."
+  })
+  res.send("Browser time!");
 });
 
+var Vendor = require("./db/connection").models.Vendor;
+var Market = require("./db/connection").models.Market;
+
+app.get("/search", function(req, res) {
+    var userMarketSearch = req.query.q
+    // search.marketSearch(req.query.q)
+    Market.findAll({
+        where: {
+            name: userMarketSearch
+        }
+    }).then(function(searchResults) {
+        console.log(searchResults)
+        if (searchResults == false) {
+            Market.findAll({
+                where: {
+                    addressZip: userMarketSearch
+                }
+            }).then(function(searchResults) {
+                res.render("markets/search", {
+                    markets: searchResults
+                })
+            })
+        } else
+            res.render("markets/search", {
+                markets: searchResults
+            })
+    })
+
+    // res.send("search complete")
+})
+
 // Index route for markets.
-app.get("/markets", function(req,res){
+app.get("/markets", function(req, res) {
   // console.log(apiRouter.sayHello())
   // Market.findAll().then(function(markets){
-    // var results = apiRouter.marketResults(20001)
-    res.render("markets/index", {message: "market route"})
+  // var results = apiRouter.marketResults(20001)
+  res.render("markets/index", {
+    message: "market route"
+  })
   // })
 });
 
 // Show route for markets.
-app.get("/markets/:id", function(req,res){
-  res.render("markets/show", {message: "Info for farmer's market " + req.params.id + "."})
+app.get("/markets/:id", function(req, res) {
+  res.render("markets/show", {
+    message: "Info for farmer's market " + req.params.id + "."
+  })
 });
-
-// Index route for vendors.
-app.get("/vendors", function(req,res){
-  // Vendor.findAll().then(function(vendors){
-    res.render("vendors/index", {message: "List of vendors."})
-  // })
-});
-
-// New route for vendors.
-app.get("/vendors/new", function(req,res){
-  res.render("vendors/new", {message: "Form for new vendor."})
-});
-
-// Show route for vendors.
-app.get("/vendors/:id", function(req,res){
-  res.render("vendors/show", {message: "Info for vendor " + req.params.id + "."})
-});
-
-// Edit route for vendors.
-app.get("/vendors/:id/edit", function(req,res){
-  res.render("vendors/edit", {message: "Edit form for vendor " + req.params.id + "."})
-});
-
-// Create new vendor route.
-// app.post("/vendors", function(req,res){
-//   Vendor.create(req.body).then(function(vendor){
-//     res.json(vendor);
-//   })
-// })
 
 // Create route for about.
-app.get('/about', function(req, res){
+app.get('/about', function(req, res) {
   res.render('about', {
     title: 'About'
   });
 });
 
 // Create route for contact.
-app.get('/contact', function(req, res){
+app.get('/contact', function(req, res) {
   res.render('contact', {
     title: 'Contact'
   });
@@ -87,6 +108,6 @@ app.get('/contact', function(req, res){
 // Port listener.
 app.set("port", (process.env.PORT || 3000));
 
-app.listen(app.get("port"), function(){
- console.log("Listening on port 3000.");
+app.listen(app.get("port"), function() {
+    console.log("Listening on port 3000.");
 });
