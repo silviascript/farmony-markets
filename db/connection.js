628cf48d0605f691ee6cf80cfec170ca20e3e22b
Silvia Brown <siltastic@gmail.com>
2015-08-27 01:54:33 -0400

    Edited comments and indentation.

--- db/connection.js
+++ db/connection.js
@@ -3,28 +3,27 @@
 var Sequelize = require("sequelize");
 // var databaseConnection = new Sequelize("postgres:///farmony_db");
 
-
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
+if (process.env.DATABASE_URL){
+  // The application is executed on Heroku, use the postgres database.
   databaseConnection = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    logging:  true //false
+    logging:  true // false
   });
 } else {
-  // the application is executed on the local machine
+  // The application is executed on the local machine.
   databaseConnection = new Sequelize("postgres:///farmony_db");
 }
 
 // Create farmers market model.
-// // Create vendors model.
+// Create vendors model.
 var Market = databaseConnection.import("../models/market");
 var Vendor = databaseConnection.import("../models/vendor");
+// var Booth = databaseConnection.import("../models/booth");
 
-// Vendors belong to many Farmers Markets.
-// Farmers Markets have many vendors.
-Vendor.belongsTo(Market);
-Market.hasMany(Vendor);
+// Many to many relationship between Vendors and Markets.
+Vendor.belongsToMany(Market, {through: 'booth'});
+Market.belongsToMany(Vendor, {through: 'booth'});
 
 // Add module exports for sequelize and models.
 module.exports = {
@@ -32,6 +31,7 @@ module.exports = {
   databaseConnection: databaseConnection,
   models: {
     Vendor: Vendor,
-    Market: Market
+    Market: Market,
+    // Booth: Booth
   }
 }
